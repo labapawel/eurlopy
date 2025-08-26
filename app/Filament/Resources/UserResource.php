fa05485b0a6f9d95d91ea67e84062ffe21fc0e61
Paweł Łaba <labapawel@gmail.com>
2025-08-26 09:59:37 +0200

    zmiany

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -150,9 +150,9 @@ class UserResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 
